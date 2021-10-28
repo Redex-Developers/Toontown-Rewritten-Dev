9485765446936aac39b49700fb8497148d0f9dc5
Drew Rogers <regdogg.acr@gmail.com>
2021-10-28 15:17:32 -0500

    Update ToontownStart.py

--- toontown/toonbase/ToontownStart.py
+++ toontown/toonbase/ToontownStart.py
@@ -1,6 +1,6 @@
 from panda3d.core import *
 
-print('Toontown Rewritten Private is an ongoing project by Redex Development')
+print ('Toontown Rewritten Private is an ongoing project by Redex Development')
 loadPrcFile('config/dev.prc')
 
 if ConfigVariableBool('want-new-ttrloader', False):
